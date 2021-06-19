041ef58bcbdf7a12811450a52e280acb8e82dd9e
MardonioMelo <mardonio.quimico@gmail.com>
2021-06-19 13:09:10 -0300

    update

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,11 @@
+<template>
+    <dir>
+        <button @click="$emit('mudarImagem')">Trocar imagem de perfil</button>
+    </dir>
+</template>
+<script>
+export default {
+    name: 'MudarImagem',
+    emits: ['mudarImagem']
+}
+</script>
--- src/components/PictureItem.vue
+++ src/components/PictureItem.vue
@@ -1,20 +1,29 @@
 <template >
   <div>
     <img :src="avatar" :alt="descricao" />
-    <reutilizacao-item/>
+    <MudarImagem @mudar-imagem="trocarImagem" />
   </div>
 </template>
 
 <script>
-import ReutilizacaoItem from './ReutilizacaoItem.vue';
+import MudarImagem from "./MudarImagem.vue";
 export default {
-  components: { ReutilizacaoItem },
+  components: { MudarImagem },
   name: "PictureItem",
   data() {
     return {
-      avatar: "./img/avatar.jpg",
+      avatar: "/img/avatar.jpg",
       descricao: "Mardônio",
     };
+  },
+  methods: {
+    trocarImagem() {      
+      if (this.avatar === "/img/avatar.jpg"){
+        this.avatar = "/img/avatar2.png";
+      }else{
+        this.avatar = "/img/avatar.jpg";
+      }      
+    },
   },
 };
 </script>
